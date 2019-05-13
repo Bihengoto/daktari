1789c57d4bc77273585c2e19f6b24f65c42a935f
Bihengoto <isaacbihengoto@gmail.com>
2019-05-13 03:43:14 +0300

    inserted text in the activity_main xml file and altered the colors and main activity file

--- app/src/main/java/com/mahugu/cdaktari/MainActivity.java
+++ app/src/main/java/com/mahugu/cdaktari/MainActivity.java
@@ -2,12 +2,16 @@ package com.mahugu.cdaktari;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+//import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+//    private Button mNextButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+//        mNextButton = (Button) findViewById(R.id.nextButton);
     }
 }
