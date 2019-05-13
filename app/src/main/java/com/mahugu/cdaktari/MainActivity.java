3041c317e71acb42cb4ca32189485226ef793490
Bihengoto <isaacbihengoto@gmail.com>
2019-05-13 04:04:50 +0300

    inserted the code for the button that deals with toasts in both files

--- app/src/main/java/com/mahugu/cdaktari/MainActivity.java
+++ app/src/main/java/com/mahugu/cdaktari/MainActivity.java
@@ -2,16 +2,24 @@ package com.mahugu.cdaktari;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-//import android.widget.Button;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-//    private Button mNextButton;
+    private Button mNextButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-//        mNextButton = (Button) findViewById(R.id.nextButton);
+        mNextButton = (Button) findViewById(R.id.nextButton);
+        mNextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Hello World!", Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
