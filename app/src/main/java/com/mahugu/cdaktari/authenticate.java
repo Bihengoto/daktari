7589946e134cef11262c1446bf6d33083f1b81ab
Bihengoto <isaacbihengoto@gmail.com>
2019-05-13 11:30:16 +0300

    made changes to the mainactivity file to fit the codes inserted in the authenticate file

--- app/src/main/java/com/mahugu/cdaktari/authenticate.java
+++ app/src/main/java/com/mahugu/cdaktari/authenticate.java
@@ -5,9 +5,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class authenticate extends AppCompatActivity {
+
     private Button mNextButton;
 
     @Override
@@ -19,7 +19,7 @@ public class MainActivity extends AppCompatActivity {
         mNextButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, authenticate.class);
+                Intent intent = new Intent(authenticate.this, authenticate.class);
                 startActivity(intent);
             }
         });
