c4ce178fe421d8874b6397532096789e86dee133
Bihengoto <isaacbihengoto@gmail.com>
2019-05-14 14:03:51 +0300

    fixed a number of bugs in the adapter file and added the code for styling the book activity

--- app/src/main/java/com/mahugu/cdaktari/MyAppointmentArrayAdapter.java
+++ app/src/main/java/com/mahugu/cdaktari/MyAppointmentArrayAdapter.java
@@ -1,30 +1,30 @@
-//package com.mahugu.cdaktari;
-//
-//import android.content.Context;
-//import android.widget.ArrayAdapter;
-//
-//public class MyAppointmentArrayAdapter extends ArrayAdapter {
-//    private Context mContext;
-//    private String[] mPatient;
-//    private String[] mDates;
-//
-//    public MyAppointmentArrayAdapter(Context mContext, int resource, String[] mPatient, String[] dates) {
-//        super(mContext, resource);
-//        this.mContext = mContext;
-//        this.mPatient = mPatient;
-//        this.mDates = dates;
-//    }
-//
-//    @Override
-//    public Object getItem(int position) {
-//        String restaurant = mPatient[position];
-//        String cuisine = mDates[position];
-//        return String.format("Patient: %s \nDate of appointment: %s", restaurant, cuisine);
-//    }
-//
-//    @Override
-//    public int getCount() {
-//        return mPatient.length;
-//    }
-//
-//}
+package com.mahugu.cdaktari;
+
+import android.content.Context;
+import android.widget.ArrayAdapter;
+
+public class MyAppointmentArrayAdapter extends ArrayAdapter {
+    private Context mContext;
+    private String[] mPatient;
+    private String[] mDates;
+
+    public MyAppointmentArrayAdapter(Context mContext, int resource, String[] mPatient, String[] dates) {
+        super(mContext, resource);
+        this.mContext = mContext;
+        this.mPatient = mPatient;
+        this.mDates = dates;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        String restaurant = mPatient[position];
+        String cuisine = mDates[position];
+        return String.format("Patient: %s \nDate of appointment: %s", restaurant, cuisine);
+    }
+
+    @Override
+    public int getCount() {
+        return mPatient.length;
+    }
+
+}
