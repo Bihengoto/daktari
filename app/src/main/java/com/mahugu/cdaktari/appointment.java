827910d9bf1e82b556286684c31e25ca61ba27ad
Bihengoto <isaacbihengoto@gmail.com>
2019-05-14 13:47:54 +0300

    changes a number of styles in the authenticate activity file and added the bind and adapter code in the appointment file

--- app/src/main/java/com/mahugu/cdaktari/appointment.java
+++ app/src/main/java/com/mahugu/cdaktari/appointment.java
@@ -15,51 +15,53 @@ import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class appointment extends AppCompatActivity implements View.OnClickListener {
-    private TextView mLocationTextView;
-    private ListView mListView;
-    @BindView(R.id.nextButtonBook)
-    Button mNext;
-
-
-    private String[] patient = new String[]{"Dennis Walter - Dr. Alex Wilson", "Cynthia Chris - Dr Hass Steve"};
-
-    private String[] dates = new String[]{"Monday 15th May 2019", "Friday 18th May 2019"};
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_appointment);
-
-        mListView = (ListView) findViewById(R.id.listView);
-        mLocationTextView = (TextView) findViewById(R.id.locationTextView);
-
-        ButterKnife.bind(this);
-
-
-        MyAppointmentArrayAdapter adapter = new MyAppointmentArrayAdapter(this, android.R.layout.simple_list_item_1, patient, dates);
-        mListView.setAdapter(adapter);
-        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                String restaurant = ((TextView) view).getText().toString();
-                Toast.makeText(appointment.this, restaurant, Toast.LENGTH_LONG).show();
-            }
-        });
-
-        Intent intent = getIntent();
-        String location = intent.getStringExtra("location");
-        mLocationTextView.setText("Patient " + location + "...\n Here are the list of patients with the respective doctors and date of appointment:");
-
-        mNext.setOnClickListener(this);
-
-    }
-
-    @Override
-    public void onClick(View v) {
-        if (v == mNext) {
-            Intent intentBook = new Intent(appointment.this, book.class);
-            startActivity(intentBook);
-        }
-
-    }
+//    private TextView mLocationTextView;
+//    private ListView mListView;
+//    @BindView(R.id.nextButtonBook)
+//    Button mNext;
+//
+//
+//    private String[] patient = new String[]{"Dennis Walter - Dr. Alex Wilson", "Janes Walter - Dr. Derrick Wilson",
+//            "Dennis Njenga - Dr. Wells Wilson", "Cynthia Chris - Dr Hass Steve"};
+//
+//    private String[] dates = new String[]{"Monday 15th May 2019 -9:00AM", "Tuesday 15th May 2019 -10:00AM", "Monday 15th May 2019 -3:00PM",
+//            "Friday 18th May 2019 -12:30PM"};
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_appointment);
+//
+//        mListView = (ListView) findViewById(R.id.listView);
+//        mLocationTextView = (TextView) findViewById(R.id.locationTextView);
+//
+//        ButterKnife.bind(this);
+//
+//
+//        MyAppointmentArrayAdapter adapter = new MyAppointmentArrayAdapter(this, android.R.layout.simple_list_item_1, patient, dates);
+//        mListView.setAdapter(adapter);
+//        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+//            @Override
+//            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+//                String restaurant = ((TextView) view).getText().toString();
+//                Toast.makeText(appointment.this, restaurant, Toast.LENGTH_LONG).show();
+//            }
+//        });
+//
+//        Intent intent = getIntent();
+//        String location = intent.getStringExtra("location");
+//        mLocationTextView.setText("Patient " + location + "...\n Here are the list of patients with the respective doctors and date of appointment:");
+//
+//        mNext.setOnClickListener(this);
+//
+//    }
+//
+//    @Override
+//    public void onClick(View v) {
+//        if (v == mNext) {
+//            Intent intentBook = new Intent(appointment.this, book.class);
+//            startActivity(intentBook);
+//        }
+//
+//    }
 }
