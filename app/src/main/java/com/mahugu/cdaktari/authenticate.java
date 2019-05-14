b8536c889e1cfba7be1ca1c38d96d85144ac9034
Bihengoto <isaacbihengoto@gmail.com>
2019-05-14 13:34:43 +0300

    changed a style in activity_main file and added the code for binding in the authenticate file

--- app/src/main/java/com/mahugu/cdaktari/authenticate.java
+++ app/src/main/java/com/mahugu/cdaktari/authenticate.java
@@ -10,32 +10,32 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
-;import butterknife.BindView;
+import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class authenticate extends AppCompatActivity implements View.OnClickListener {
 
-    @BindView(R.id.nextButtonAuth) Button mFindRestaurantsButton;
-    @BindView(R.id.locationEditText) EditText mLocationEditText;
-    @BindView(R.id.appNameTextView)
-    TextView mAppNameTextView;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_authenticate);
-        ButterKnife.bind(this);
-
-        mFindRestaurantsButton.setOnClickListener(this);
-    }
-            @Override
-            public void onClick(View v) {
-                if(v == mFindRestaurantsButton){
-                String location = mLocationEditText.getText().toString();
-                Intent intent = new Intent(authenticate.this, appointment.class);
-                intent.putExtra("location", location);
-                startActivity(intent);
-            }
-        }
+//    @BindView(R.id.nextButtonAuth) Button mFindRestaurantsButton;
+//    @BindView(R.id.locationEditText) EditText mLocationEditText;
+//    @BindView(R.id.appNameTextView)
+//    TextView mAppNameTextView;
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_authenticate);
+//        ButterKnife.bind(this);
+//
+//        mFindRestaurantsButton.setOnClickListener(this);
+//    }
+//            @Override
+//            public void onClick(View v) {
+//                if(v == mFindRestaurantsButton){
+//                String location = mLocationEditText.getText().toString();
+//                Intent intent = new Intent(authenticate.this, appointment.class);
+//                intent.putExtra("location", location);
+//                startActivity(intent);
+//            }
+//        }
 }
 
