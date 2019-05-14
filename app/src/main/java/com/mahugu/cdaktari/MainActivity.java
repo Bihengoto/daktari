a431050bfacf1c42a6e76bbde239b2d0a90d82fa
Bihengoto <isaacbihengoto@gmail.com>
2019-05-14 13:30:05 +0300

    solved a bug in mainactivity file and styled the button in activity_main file

--- app/src/main/java/com/mahugu/cdaktari/MainActivity.java
+++ app/src/main/java/com/mahugu/cdaktari/MainActivity.java
@@ -4,14 +4,14 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-//import android.widget.Button;
-//import android.widget.Toast;
-//
-//import butterknife.BindView;
+import android.widget.Button;
+import android.widget.Toast;
+
+import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity implements  View.OnClickListener{
-//    @BindView(R.id.nextButton) Button mNext;
+    @BindView(R.id.nextButton) Button mNext;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,14 +19,14 @@ public class MainActivity extends AppCompatActivity implements  View.OnClickList
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
 
-//        mNext.setOnClickListener(this);
+       mNext.setOnClickListener(this);
     }
 
-//    @Override
-//    public void onClick(View v) {
-//        if(v == mNext){
-//            Intent intent = new Intent(MainActivity.this,authenticate.class);
-//            startActivity(intent);
-//        }
-//    }
+    @Override
+    public void onClick(View v) {
+        if(v == mNext){
+            Intent intent = new Intent(MainActivity.this,authenticate.class);
+            startActivity(intent);
+        }
+    }
 }
