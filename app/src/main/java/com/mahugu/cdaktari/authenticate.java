6d3247b657d414b48c4169cdeaee1539ac6135ca
Bihengoto <isaacbihengoto@gmail.com>
2019-05-14 13:39:19 +0300

    fixed bugs in authenticate file and styled the authenticate file in activity_authenticate file

--- app/src/main/java/com/mahugu/cdaktari/authenticate.java
+++ app/src/main/java/com/mahugu/cdaktari/authenticate.java
@@ -1,10 +1,8 @@
 package com.mahugu.cdaktari;
 
 import android.content.Intent;
-import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -15,27 +13,27 @@ import butterknife.ButterKnife;
 
 public class authenticate extends AppCompatActivity implements View.OnClickListener {
 
-//    @BindView(R.id.nextButtonAuth) Button mFindRestaurantsButton;
-//    @BindView(R.id.locationEditText) EditText mLocationEditText;
-//    @BindView(R.id.appNameTextView)
-//    TextView mAppNameTextView;
-//
-//    @Override
-//    protected void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_authenticate);
-//        ButterKnife.bind(this);
-//
-//        mFindRestaurantsButton.setOnClickListener(this);
-//    }
-//            @Override
-//            public void onClick(View v) {
-//                if(v == mFindRestaurantsButton){
-//                String location = mLocationEditText.getText().toString();
-//                Intent intent = new Intent(authenticate.this, appointment.class);
-//                intent.putExtra("location", location);
-//                startActivity(intent);
-//            }
-//        }
+    @BindView(R.id.nextButtonAuth) Button mnextButtonAuth;
+    @BindView(R.id.locationEditText) EditText mLocationEditText;
+    @BindView(R.id.appNameTextView)
+    TextView mAppNameTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_authenticate);
+        ButterKnife.bind(this);
+
+        mnextButtonAuth.setOnClickListener(this);
+    }
+            @Override
+            public void onClick(View v) {
+                if(v == mnextButtonAuth){
+                String location = mLocationEditText.getText().toString();
+                Intent intent = new Intent(authenticate.this, appointment.class);
+                intent.putExtra("location", location);
+                startActivity(intent);
+            }
+        }
 }
 
